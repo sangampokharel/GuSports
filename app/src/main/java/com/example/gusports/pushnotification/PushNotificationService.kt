4778536b9c53b97585cc33223d8f2416b7b76af8
sangam <sangampokharel803@gmail.com>
2022-11-22 17:59:35 +0530

    Implemented Push Notification

---
+++ app/src/main/java/com/example/gusports/pushnotification/PushNotificationService.kt
@@ -0,0 +1,68 @@
+package com.example.gusports.pushnotification
+
+import android.R
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.example.gusports.ui.MainActivity
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+class PushNotificationService:FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d("TOKETN", "onNewToken: $token")
+
+
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        addNotification(message.notification!!.title, message.notification!!.body)
+    }
+
+
+    private fun addNotification(title:String?,body:String?) {
+        lateinit var notificationChannel: NotificationChannel
+        lateinit var builder: Notification.Builder
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        var notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel = NotificationChannel("guapps", "Notifications", NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.GREEN
+            notificationChannel.enableVibration(false)
+            notificationManager.createNotificationChannel(notificationChannel)
+
+            builder = Notification.Builder(this, "guapps")
+                .setSmallIcon(R.drawable.ic_notification_clear_all)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentIntent(pendingIntent)
+        } else {
+
+            builder = Notification.Builder(this)
+                .setSmallIcon(R.drawable.ic_notification_clear_all)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentIntent(pendingIntent)
+        }
+        notificationManager.notify(1234, builder.build())
+    }
+
+}
--- app/src/main/java/com/example/gusports/ui/MainActivity.kt
+++ app/src/main/java/com/example/gusports/ui/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.gusports.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.gusports.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
+
+
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +22,6 @@ class MainActivity : AppCompatActivity() {
         val navController = this.findNavController(R.id.fragmentContainerView)
         val navView: BottomNavigationView = bottomNavigationView
         navView.setupWithNavController(navController)
+
     }
 }
